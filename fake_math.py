191723ee21737e00087c401ed23228e09d48f74c
AlcheMystic <pashkinms86@gmail.com>
2024-09-19 21:03:58 +0700

    practice1 complete

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+
+def div(first, second):
+    if second == 0:
+        return 'Error div_0'
+    else:
+        return first / second
---
+++ true_math.py
@@ -0,0 +1,5 @@
+def div(first, second):
+    if second == 0:
+        return 'Inf.'
+    else:
+        return first / second
